961ef0926954a8c362335da831d28f52096cb334
rgarro <rgarro@gmail.com>
2020-09-10 19:34:08 -0600

    Initial Commit

---
+++ MyFirstProject/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MyFirstProject
+//
+//  Created by Rolando Garro on 9/10/20.
+//  Copyright © 2020 emptyArt. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ MyFirstProject/AppDelegate.m
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.m
+//  MyFirstProject
+//
+//  Created by Rolando Garro on 9/10/20.
+//  Copyright © 2020 emptyArt. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
