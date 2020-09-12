872f92f10e0b49636d0d1dccc9219b9ca3355475
rgarro <rgarro@gmail.com>
2020-09-11 20:51:52 -0600

    convulsing enter file ...

---
+++ MyFirstProject/Fusebox.h
@@ -0,0 +1,13 @@
+//
+//  Fusebox.h
+//  MyFirstProject
+//
+//  Created by Rolando Garro on 9/11/20.
+//  Copyright © 2020 emptyArt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Fusebox : NSObject
+
+@end
---
+++ MyFirstProject/Fusebox.m
@@ -0,0 +1,13 @@
+//
+//  Fusebox.m
+//  MyFirstProject
+//
+//  Created by Rolando Garro on 9/11/20.
+//  Copyright © 2020 emptyArt. All rights reserved.
+//
+
+#import "Fusebox.h"
+
+@implementation Fusebox
+
+@end
